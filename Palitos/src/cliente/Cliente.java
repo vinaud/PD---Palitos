e264954faadf38e008c268a76c804d7f81e23c93
joaovinaud <joaovinaud@VInaud>
2016-05-12 15:00:17 -0300

    inicio da logica do login do lado cliente

--- Palitos/src/cliente/Cliente.java
+++ Palitos/src/cliente/Cliente.java
@@ -1,9 +1,32 @@
 package cliente;
 
+import java.util.Scanner;
+
 public class Cliente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan= new Scanner(System.in);
+		String user ="";
+		String senha ="";
+		boolean logado = false;
+		do
+		{
+			System.out.println("Insira o usuario");
+			 user = scan.nextLine();
+			
+			System.out.println("Insira a senha");
+			 senha = scan.nextLine();
+			
+			logado = LoginCliente.login(user,senha);
+			if(!logado)
+			{
+			System.out.println("Usuario ou senha invalido");
+			}
+			
+		}while (!logado);
+		
+		System.out.println("Login efetuado com sucesso");
+		
 
 	}
 
--- Palitos/src/cliente/LoginCliente.java
+++ Palitos/src/cliente/LoginCliente.java
@@ -1,5 +1,10 @@
 package cliente;
 
 public class LoginCliente {
+	
+	public static boolean login(String user, String senha)
+	{
+		return false;
+	}
 
 }
