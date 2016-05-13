f49500e8f8ba61ba2dc1b04b3c28815fa444371e
joaovinaud <joaovinaud@VInaud>
2016-05-13 14:43:54 -0300

    main do servidor

--- Palitos/src/servidor/ServidorMain.java
+++ Palitos/src/servidor/ServidorMain.java
@@ -1,9 +1,27 @@
 package servidor;
 
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
 public class ServidorMain {
 
+	public ServidorMain() throws RemoteException
+	{
+		super();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try
+		{
+			System.setSecurityManager(new SecurityManager());
+			ServicoLogin sl = new ServicoLogin();
+			Naming.rebind("//localhost/login", sl);
+			System.out.println("Servifor iniciado");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Falha ao iniciar o servidor " + e);
+		}
 
 	}
 
